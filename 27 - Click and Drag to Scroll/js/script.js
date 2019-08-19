c2b8737218c815b52930982d3a053c2e64fc2218
EduardoSorokin <eduardosorokin@gmail.com>
2019-08-19 11:24:53 -0300

    Day 27 - Click and Drag to Scroll

---
+++ 27 - Click and Drag to Scroll/js/script.js
@@ -0,0 +1,38 @@
+const slider = document.querySelector('.items')
+let isDown = false
+let startX
+let scrollLeft
+
+function handleMouseDown(e) {
+  isDown = true
+  slider.classList.add('active')
+  startX = e.pageX - slider.offsetLeft
+  scrollLeft = slider.scrollLeft
+}
+
+function handleMouseLeave() {
+  isDown = false
+  slider.classList.remove('active')
+}
+
+function handleMouseUp() {
+  isDown = false
+  slider.classList.remove('active')
+}
+
+function handleMouseMove(e) {
+  if (!isDown) {
+    return // Stop the function from running
+  }
+
+  e.preventDefault()
+
+  const x = e.pageX - slider.offsetLeft
+  const walk = (x - startX) * 3
+  slider.scrollLeft = scrollLeft - walk
+}
+
+slider.addEventListener('mousedown', handleMouseDown)
+slider.addEventListener('mouseleave', handleMouseLeave)
+slider.addEventListener('mouseup', handleMouseUp)
+slider.addEventListener('mousemove', handleMouseMove)
